de49748bcf3734c07624042f819c5c3c56f3e59d
Angel Gonzalez <angel@capeandbay.com>
2021-07-22 05:01:27 -0400

    packages clogging the pipes

--- packages/capeandbay-devs/nautical-integrator/config/nautical-integrator.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'jwt_secret' => env('JWT_SECRET', 'bufu'),
-    'root_default_route' => 'errors.404',
-    'anchor_cms_url' => env('ANCHOR_CMS_URL', 'https://anchor.capeandbay.com')
-];
--- packages/capeandbay-devs/nautical-integrator/src/NauticalIntegrator.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace AnchorCMS\NauticalIntegrator;
-
-class NauticalIntegrator {
-
-}
